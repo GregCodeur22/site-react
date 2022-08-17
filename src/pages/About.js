46ab6f12cd554d47426206e16841e4afa2b9ecd8
Grégory <gregory.arside@gmail.com>
2022-08-17 19:46:21 +0200

    exo react

---
+++ src/pages/About.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1> A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam amet
+        facere culpa consequatur ipsa? Esse distinctio praesentium corporis
+        aspernatur quae aut vel iusto cum explicabo, nemo dolor nam consequatur
+        ipsum omnis saepe ipsam quia laboriosam similique voluptatem voluptates
+        vero? Illo perferendis, incidunt eos, amet consequuntur aspernatur
+        voluptates porro odit, ducimus asperiores vitae! Praesentium excepturi
+        tempora veritatis tempore. Reiciendis perferendis eos eaque, architecto
+        porro excepturi recusandae. Repellendus magni totam, deserunt itaque
+        quaerat nam iusto! Maiores voluptatum, numquam soluta provident quas
+        quae omnis, recusandae minus iure eius quidem maxime, odit mollitia
+        saepe veritatis modi ratione. Maiores assumenda unde nobis, error
+        eligendi deserunt.
+      </p>
+
+      <br />
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, qui.
+        Doloremque dignissimos, suscipit fuga recusandae libero aliquid quaerat!
+        Quasi nihil, ab veniam molestiae nemo eius eveniet dolorum labore
+        tenetur at placeat quis animi doloremque aut architecto facere ea quam
+        sint explicabo totam pariatur deserunt incidunt repudiandae vitae. Iure
+        ex magnam eaque assumenda eum cupiditate, explicabo officiis eius
+        quibusdam! Natus delectus corrupti, nulla expedita provident ipsum
+        commodi vel doloremque et, officia explicabo possimus blanditiis sint,
+        id facilis magni ratione ex ea voluptatibus voluptates quia facere velit
+        cumque aliquam. Provident fugiat sed repellendus minima consequuntur
+        repudiandae tempora sequi, maxime quasi quos, libero beatae tempore.
+        Corrupti reiciendis mollitia earum corporis, dolor numquam inventore
+        ipsam accusantium at optio omnis esse a recusandae eligendi! Explicabo
+        corporis sed et corrupti expedita voluptatum autem sunt quaerat labore,
+        eius placeat iusto unde, blanditiis libero, id mollitia voluptatem
+        doloremque voluptate distinctio quae aliquam. Eveniet corrupti nemo
+        beatae dolores iure explicabo voluptatum, unde voluptates? Deleniti
+        debitis modi reprehenderit magni praesentium porro, cupiditate corrupti
+        saepe quae impedit ab necessitatibus? Id maxime, impedit eius facere
+        eligendi facilis possimus nesciunt modi architecto necessitatibus ipsum
+        numquam ex fuga illo mollitia, animi non repellat aliquid et delectus.
+        Sint eos cupiditate officia perferendis optio repellendus vel!
+      </p>
+    </div>
+  );
+};
+
+export default About;
